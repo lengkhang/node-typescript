d02a2f48c5853d0b6d92cdcfc8134bbb610accf5
lengkhang <lengkhang@gmail.com>
2023-08-26 15:48:56 +0800

    Initial

---
+++ dist/greeter.js
@@ -0,0 +1,6 @@
+"use strict";
+const hello = (firstName) => {
+    return `Hello ${firstName}!`;
+};
+const userName = 'Andrew';
+console.log(hello(userName));
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+const hello = (firstName: string) => {
+  return `Hello ${firstName}!`;
+};
+
+const userName = 'Andrew';
+
+console.log(hello(userName));
